c19a78bad4076c1ae5a83018bb7a1c09cae78722
Robson de Moraes Silva <rms2307@outlook.com>
2023-01-16 15:19:36 -0300

    Router link - Navegando entre rotas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,6 @@
 <template>
+  <div>
     <h3>Home</h3>
+    <router-link to="/login">Ir para Login</router-link>
+  </div>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,6 @@
 <template>
+  <div>
     <h3>Login</h3>
+    <router-link :to="{ path: '/home' }">Ir para Home</router-link>
+  </div>
 </template>
